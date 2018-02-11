ffe465fa448a45a5446d498c53438922103387d8
Stuart Mesham <stumes3@gmail.com>
2018-02-11 11:55:41 +0200

    Made file reading branch

--- Main.py
+++ Main.py
@@ -1,11 +1,7 @@
-for i in range(n_endpoints):
-    # read data for each endpoint
-    pass
-
-
 class Endpoint(object):
     def __init__(self, datacentre_latency):
         self.datacentre_latency = datacentre_latency
+        self.caches = []
         self.requests = []
 
 
@@ -26,15 +22,18 @@ class Video(object):
     def __init__(self, id, size):
         self.id = id
         self.size = size
-
-
+        
+        
 def main():
-	connection_latencies = [[]] # where connection[cache_id][endpoint_id] is the latency between cache and endpoint
-	
     # read data from file
     f = open('me_at_the_zoo.in')
     n_videos, n_endpoints, n_request_descriptions, n_caches, cache_size = [int(part) for part in f.readline().split()]
     video_sizes = [int(part) for part in f.readline().split()]
+    
+    for i in range(n_endpoints):
+        # read data for each endpoint
+        datacentre_latency, endpoint_n_caches
+        pass
     
     f.close()
 
