b74189e23eef2efb0db4b4f7ebdc97e07a2d7dd3
Stuart Mesham <stumes3@gmail.com>
2018-02-11 12:56:18 +0200

    File reading bug fix

--- Main.py
+++ Main.py
@@ -1,3 +1,10 @@
+endpoints = []
+caches = []
+videos = []
+requests = []
+
+connection_latencies = [[]]  # where connection[cache_id][endpoint_id] is the latency between cache and endpoint
+
 class Endpoint(object):
     def __init__(self, datacentre_latency):
         self.datacentre_latency = datacentre_latency
@@ -22,6 +29,43 @@ class Video(object):
         self.size = size
 
 
+def read_file(filename):
+    f = open(filename)
+    n_videos, n_endpoints, n_request_descriptions, n_caches, cache_size = [int(part) for part in f.readline().split()]
+    video_sizes = [int(part) for part in f.readline().split()]
+    
+    for i in range(n_videos):
+        videos.append(Video(video_sizes[i]))
+    
+    for i in range(n_caches):
+        caches.append(Cache())
+    
+    for i in range(n_caches):
+        connection_latencies.append([])
+        for j in range(n_endpoints):
+            connection_latencies[i].append([])
+    
+    for i in range(n_endpoints):
+        # read data for each endpoint
+        datacentre_latency, endpoint_n_caches = [int(part) for part in f.readline().split()]
+        
+        endpoints.append(Endpoint(datacentre_latency))
+        
+        for j in range(endpoint_n_caches):
+            cache, latency = [int(part) for part in f.readline().split()]
+            endpoints[i].caches.append(caches[cache])
+            caches[cache].endpoints.append(endpoints[i])
+            connection_latencies[cache][i] = latency
+    
+    for i in range(n_request_descriptions):
+        # read data for each video
+        video_id, endpoint_id, n_requests = [int(part) for part in f.readline().split()]
+        
+        requests.append(Request(n_requests, videos[video_id]))
+        endpoints[endpoint_id].requests.append(requests[i])
+    
+    f.close()
+
 def strip_videos(videos, cache_size, requests):
     """ Remove videos that are unrequested
     remove videos that are too large for any of the data centres"""
@@ -39,49 +83,9 @@ def strip_videos(videos, cache_size, requests):
 
 
 def main():
-    endpoints = []
-    caches = []
-    videos = []
-    requests = []
     
-    connection_latencies = [[]] # where connection[cache_id][endpoint_id] is the latency between cache and endpoint
+    read_file('example.in')
     
-    # read data from file
-    f = open('me_at_the_zoo.in')
-    n_videos, n_endpoints, n_request_descriptions, n_caches, cache_size = [int(part) for part in f.readline().split()]
-    video_sizes = [int(part) for part in f.readline().split()]
-    
-    for i in range(n_videos):
-        videos.append(Video(video_sizes[i]))
-    
-    for i in range(n_caches):
-        caches.append(Cache())
-        
-    for i in range(n_caches):
-        connection_latencies.append([])
-        for j in range(n_endpoints):
-            connection_latencies[i].append([])
-
-    for i in range(n_endpoints):
-        # read data for each endpoint
-        datacentre_latency, endpoint_n_caches = [int(part) for part in f.readline().split()]
-        
-        endpoints.append(Endpoint(datacentre_latency))
-        
-        for j in range(endpoint_n_caches):
-            cache, latency = [int(part) for part in f.readline().split()]
-            endpoints[i].caches.append(caches[cache])
-            caches[cache].endpoints.append(endpoints[i])
-            connection_latencies[cache][i] = latency
-            
-    for i in range(n_videos):
-        # read data for each video
-        video_id, endpoint_id, n_requests = [int(part) for part in f.readline().split()]
-        
-        requests.append(Request(n_requests, videos[video_id]))
-        endpoints[endpoint_id].requests.append(requests[i])
-    
-    f.close()
 
     #Strip unneeded videos
     videos = strip_videos()
