c7f7faa58031d1e743f261e60243f55691543678
Stuart Mesham <stumes3@gmail.com>
2018-02-11 11:55:41 +0200

    Made file reading branch

--- Main.py
+++ Main.py
@@ -1,19 +1,8 @@
-# read data from file
-f = open('me_at_the_zoo.in')
-n_videos, n_endpoints, n_request_descriptions, n_caches, cache_size = [int(part) for part in f.readline().split()]
-video_sizes = [int(part) for part in f.readline().split()]
-
-for i in range(n_endpoints):
-    # read data for each endpoint
-    pass
-
-
 class Endpoint(object):
-    def __init__(self, id, latency, caches, requests):
-        self.id = id
-        self.latency = latency
-        self.caches = caches
-        self.requests = requests
+    def __init__(self, datacentre_latency):
+        self.datacentre_latency = datacentre_latency
+        self.caches = []
+        self.requests = []
 
 
 class Cache(object):
@@ -32,3 +21,21 @@ class Video(object):
     def __init__(self, id, size):
         self.id = id
         self.size = size
+        
+        
+def main():
+    # read data from file
+    f = open('me_at_the_zoo.in')
+    n_videos, n_endpoints, n_request_descriptions, n_caches, cache_size = [int(part) for part in f.readline().split()]
+    video_sizes = [int(part) for part in f.readline().split()]
+    
+    for i in range(n_endpoints):
+        # read data for each endpoint
+        datacentre_latency, endpoint_n_caches
+        pass
+    
+    f.close()
+
+
+if __name__ == '__main__':
+    main()
