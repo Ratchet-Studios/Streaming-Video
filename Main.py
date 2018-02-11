1acf0173692c14b2c59640c81e23bcb03a8b86cc
Stuart Mesham <stumes3@gmail.com>
2018-02-11 12:37:41 +0200

    Made progress on file reading

--- Main.py
+++ Main.py
@@ -6,9 +6,8 @@ class Endpoint(object):
 
 
 class Cache(object):
-    def __init__(self, id, videos):
-        self.id = id
-        self.videos = videos
+    def __init__(self):
+        self.videos = []
         self.endpoints = []
 
 
@@ -19,20 +18,23 @@ class Request(object):
 
 
 class Video(object):
-    def __init__(self, id, size):
-        self.id = id
+    def __init__(self, size):
         self.size = size
 
 
 def main():
     endpoints = []
     caches = []
+    videos = []
     connection_latencies = [[]] # where connection[cache_id][endpoint_id] is the latency between cache and endpoint
     
     # read data from file
     f = open('me_at_the_zoo.in')
     n_videos, n_endpoints, n_request_descriptions, n_caches, cache_size = [int(part) for part in f.readline().split()]
     video_sizes = [int(part) for part in f.readline().split()]
+    
+    for i in range(n_videos):
+        videos.append(Video(video_sizes[i]))
     
     for i in range(n_caches):
         caches.append(Cache())
@@ -46,8 +48,11 @@ def main():
         for j in range(endpoint_n_caches):
             cache, latency = [int(part) for part in f.readline().split()]
             endpoints[i].caches.append(caches[cache])
-            connection_latencies[cache][i] = latency
+            # connection_latencies[cache][i] = latency TODO: FIX THIS
             
+    for i in range(n_videos):
+        # read data for each video
+        video_id, endpoint_id, n_requests = [int(part) for part in f.readline().split()]
         
     
     f.close()
