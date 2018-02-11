ce1266293fa48866ace50b414f9a9bcdfc0997ba
Stuart Mesham <stumes3@gmail.com>
2018-02-11 12:24:27 +0200

    Made progress on file reading

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ class Endpoint(object):
     def __init__(self, datacentre_latency):
         self.datacentre_latency = datacentre_latency
         self.requests = []
+        self.caches = []
 
 
 class Cache(object):
@@ -29,17 +30,30 @@ class Video(object):
 
 
 def main():
-	connection_latencies = [[]] # where connection[cache_id][endpoint_id] is the latency between cache and endpoint
-	
+    endpoints = []
+    caches = []
+    connection_latencies = [[]] # where connection[cache_id][endpoint_id] is the latency between cache and endpoint
+    
     # read data from file
     f = open('me_at_the_zoo.in')
     n_videos, n_endpoints, n_request_descriptions, n_caches, cache_size = [int(part) for part in f.readline().split()]
     video_sizes = [int(part) for part in f.readline().split()]
+    
+    for i in range(n_caches):
+        caches.append(Cache())
 
-	for i in range(n_endpoints):
-		# read data for each endpoint
-		datacentre_latency, endpoint_n_caches
-		pass
+    for i in range(n_endpoints):
+        # read data for each endpoint
+        datacentre_latency, endpoint_n_caches = [int(part) for part in f.readline().split()]
+        
+        endpoints.append(Endpoint(datacentre_latency))
+        
+        for j in range(endpoint_n_caches):
+            cache, latency = [int(part) for part in f.readline().split()]
+            endpoints[i].caches.append(caches[cache])
+            connection_latencies[cache][i] = latency
+            
+        
     
     f.close()
 
