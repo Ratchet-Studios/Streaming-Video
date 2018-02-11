e72058d44a767171cb60415c1d9ec056a03d406f
Stuart Mesham <stumes3@gmail.com>
2018-02-11 12:13:42 +0200

    Made changes to classes

--- Main.py
+++ Main.py
@@ -4,17 +4,16 @@ for i in range(n_endpoints):
 
 
 class Endpoint(object):
-    def __init__(self, id, latency, caches, requests):
-        self.id = id
-        self.latency = latency
-        self.caches = caches
-        self.requests = requests
+    def __init__(self, datacentre_latency):
+        self.datacentre_latency = datacentre_latency
+        self.requests = []
 
 
 class Cache(object):
     def __init__(self, id, videos):
         self.id = id
         self.videos = videos
+        self.endpoints = []
 
 
 class Request(object):
@@ -30,6 +29,8 @@ class Video(object):
 
 
 def main():
+	connection_latencies = [[]] # where connection[cache_id][endpoint_id] is the latency between cache and endpoint
+	
     # read data from file
     f = open('me_at_the_zoo.in')
     n_videos, n_endpoints, n_request_descriptions, n_caches, cache_size = [int(part) for part in f.readline().split()]
